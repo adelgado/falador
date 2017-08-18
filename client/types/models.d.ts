a380aaaf4b6c4f4fb0d7e6ac95c625c1b5c02048
Alessandro Delgado <adelgado1313@gmail.com>
2017-08-18 10:59:40 +0200

    Remove last boilerplate leftovers

--- client/types/models.d.ts
+++ client/types/models.d.ts
@@ -1,15 +1,3 @@
-/** TodoMVC model definitions **/
-
-interface TodoItemData {
-	id?: TodoItemId
-	text?: string
-	completed?: boolean
-}
-
-declare type TodoItemId = number
-
-declare type TodoFilterType = 'SHOW_ALL' | 'SHOW_ACTIVE' | 'SHOW_COMPLETED'
-
 declare type ChatStoreState = {
 	room: Room | null
 	loading: boolean
