2764f322ad0d92c21fda5b583fea090a1fb8fc4c
Alessandro Delgado <adelgado1313@gmail.com>
2017-08-18 04:05:43 +0200

    Remove todo-related actions

--- client/src/actions/chat.ts
+++ client/src/actions/chat.ts
@@ -1,11 +1,5 @@
 import { createAction } from 'redux-actions'
 import * as Actions from '../constants/actions'
 
-export const addTodo = createAction<TodoItemData>(Actions.ADD_TODO)
-export const editTodo = createAction<TodoItemData>(Actions.EDIT_TODO)
-export const deleteTodo = createAction<TodoItemId>(Actions.DELETE_TODO)
-export const completeTodo = createAction<TodoItemId>(Actions.COMPLETE_TODO)
-export const completeAll = createAction(Actions.COMPLETE_ALL)
-export const clearCompleted = createAction(Actions.CLEAR_COMPLETED)
 export const createRoom = createAction(Actions.CREATE_ROOM)
 export const createRoomAsync = createAction(Actions.CREATE_ROOM_ASYNC)
--- client/src/constants/actions.ts
+++ client/src/constants/actions.ts
@@ -1,8 +1,2 @@
-export const ADD_TODO = 'ADD_TODO'
-export const EDIT_TODO = 'EDIT_TODO'
-export const DELETE_TODO = 'DELETE_TODO'
-export const COMPLETE_TODO = 'COMPLETE_TODO'
-export const COMPLETE_ALL = 'COMPLETE_ALL'
-export const CLEAR_COMPLETED = 'CLEAR_COMPLETED'
 export const CREATE_ROOM = 'CREATE_ROOM'
 export const CREATE_ROOM_ASYNC = 'CREATE_ROOM_ASYNC'
